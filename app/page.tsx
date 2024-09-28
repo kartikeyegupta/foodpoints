470eeeba560df0dac051936e42352322dab50a85
Kartikeye Gupta <kartikeye@Kartikeyes-MacBook-Pro.local>
2024-09-28 04:27:33 -0400

    reset for learning

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,1 @@
-// pages/index.tsx
-"use client";
-import MathComponent from './components/MathComponent';
 
-const Home = () => {
-  return (
-    <div className="container mx-auto p-4">
-      <h1 className="text-xl font-bold mb-4">Math Calculator</h1>
-      <MathComponent />
-    </div>
-  );
-};
-
-export default Home;
